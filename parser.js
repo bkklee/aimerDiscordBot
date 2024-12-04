9a934ad3942af5c70c5eb86e6289d22dddc457fb
kklee <kklee@cse.cuhk.edu.hk>
2024-12-04 14:19:35 +0800

    Continue fix

--- parser.js
+++ parser.js
@@ -3,9 +3,10 @@ const HTMLParser = require('node-html-parser');
 const parser = (body) => {
     const htmlfile = HTMLParser.parse(body);
 
-    if(htmlfile.querySelector('.js-quote-price-root')){
-        let curPrice = htmlfile.querySelector('.js-quote-price-root').firstChild.firstChild.nextSibling.nextSibling.firstChild.text
-        let change = htmlfile.querySelector('.js-quote-price-root').firstChild.firstChild.nextSibling.nextSibling.firstChild.nextSibling.firstChild.firstChild.text.replaceAll("Dollar change","")
+    if(htmlfile.querySelector('.quote-price_wrapper_price')){
+        let curPrice = htmlfile.querySelector('.quote-price_wrapper_price').text
+        const rawChange = htmlfile.querySelector('.quote-price_wrapper_change').text.replaceAll("\n","").replaceAll(" ","")
+        const change = rawChange.match(/Dollarchange([+-]?\d+(\.\d+)?)Percentagechange/)[1];
         let changeNum = parseFloat(change.replaceAll(',', ''));
         let curPriceNum = parseFloat(curPrice.replaceAll(',', ''));
         let percentage = (changeNum / (curPriceNum - changeNum)) * 100;
