47479e7627460745669d60a0f9636de55897b9c0
kklee <kklee@cse.cuhk.edu.hk>
2024-12-04 12:10:26 +0800

    Fix URL

--- fetcher.js
+++ fetcher.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const fetcher = async (code) => {
     const get_options = {
-        url: `https://finance.yahoo.com/quote/${encodeURIComponent(code)}/chart`,
+        url: `https://finviz.com/quote.ashx?t=${encodeURIComponent(code)}&p=d`,
         headers: {
             'User-Agent':
                 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
